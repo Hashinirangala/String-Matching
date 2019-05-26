78f7f245d6c3713e534026af41b1b0220ebb6727
chalanika <chalanikasewwandi@gmail.com>
2019-05-26 10:42:50 +0530

    test1 completed

--- Question1.py
+++ Question1.py
@@ -1,4 +1,23 @@
+
 def search(pat, txt): 
-    M = len(pat) 
-    N = len(txt) 
-  
+	M = len(pat) 
+	N = len(txt) 
+
+	# A loop to slide pat[] one by one */ 
+	for i in range(N - M + 1): 
+		j = 0
+		
+		# For current index i, check 
+		# for pattern match */ 
+		for j in range(0, M): 
+			if ((txt[i + j] != pat[j]) and (pat[j]!="_")): 
+				break
+
+		if (j == M - 1): 
+			print("Pattern found at index ", i) 
+
+# Driver Code 
+if __name__ == '__main__': 
+	txt = "AABAACAADAABAAABAA"
+	pat = "A_BA"
+	search(pat, txt) 
