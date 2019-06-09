811eb0ceef2300598e58e55abf06418de7bc027e
chalanika <chalanikasewwandi@gmail.com>
2019-06-09 12:01:58 +0530

    add c

--- Question1.py
+++ Question1.py
@@ -5,19 +5,16 @@ def search(pat, txt):
 	c = 0
 	# A loop to slide pat[] one by one */ 
 	for i in range(N - M + 1): 
-		j = 0
+		j=0
 
 		# For current index i, check 
 		# for pattern match */ 
 		for j in range(0, M): 
 			if ((txt[i + j] != pat[j]) and (pat[j]!="_")): 
 				break
-
-		if (j == M - 1):
+        if(j == M-1):
             c += 1
             print(i)
-
-
 f = open("test.txt", "r")
 f1 = f.readlines()
 k = 1
